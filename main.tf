ade983a7479626c2394520eb3c390ab3bd3dc20d
fr3drickson <onargo@gmail.com>
2025-06-03 14:12:55 +0300

    Last commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-central-1"
+  region = "eu-north-1"
 }
 
 terraform {
@@ -10,14 +10,38 @@ terraform {
   }
 }
 
-data "aws_key_pair" "deployer" {
-  key_name = "lab45"
+resource "aws_key_pair" "deployer" {
+  key_name   = "lab45"
+  public_key = file("lab45.pub") # обов'язково мати цей файл поруч
 }
 
-data "aws_security_group" "web_sg" {
-  id = "sg-05780b3263dc33056"
+resource "aws_security_group" "web_sg" {
+  name        = "lab45-web-sg"
+  description = "Allow HTTP inbound traffic"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-data "aws_instance" "web_server" {
-  instance_id = "i-0ea450bf27db0b30f"
+resource "aws_instance" "web_server" {
+  ami                         = "ami-05d3e0186c058c4dd" 
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Lab45Instance"
+  }
 }
